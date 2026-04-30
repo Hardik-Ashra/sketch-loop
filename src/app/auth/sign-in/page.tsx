4861337f87e09c26254f2a08c370e7b6b57231f5
Hardik-Ashra <hardikashra1234@gmail.com>
2026-04-30 23:08:23 +0530

    feat: implement sign-in page, add OpenTelemetry Winston transport, and update TypeScript JSX configuration

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -128,7 +128,7 @@ export default function SignIn() {
 
                 <div className="p-3">
                     <p className="text-accent-foreground text-center text-sm">
-                        Don't have an account ?
+                        Don&apos;t have an account ?
                         <Button
                             asChild
                             variant="link"
